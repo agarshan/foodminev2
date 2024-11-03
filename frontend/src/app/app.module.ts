cd81bcfcab552aff07128cb9d67c09fa80112606
agarshan <agarshanbalan@gmail.com>
2024-11-03 10:30:00 +0530

    food page implemented

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,9 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/partials/header/header.component';
 import { HomeComponent } from './components/pages/home/home.component';
-import { RatingModule } from 'ng-starrating';
+import { SearchComponent } from './components/partials/search/search.component';
+import { FoodPageComponent } from './components/pages/food-page/food-page.component';
+import { TagsComponent } from './components/partials/tags/tags.component';
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, SearchComponent, FoodPageComponent, TagsComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { FoodService } from 'src/app/services/food.service';
 import { Food } from 'src/app/shared/models/Food';
 
@@ -9,7 +10,20 @@ import { Food } from 'src/app/shared/models/Food';
 })
 export class HomeComponent {
   foods: Food[] = [];
-  constructor(private foodService: FoodService) {
-    this.foods = this.foodService.getAll();
+  constructor(
+    private foodService: FoodService,
+    activatedRoute: ActivatedRoute
+  ) {
+    activatedRoute.params.subscribe((params) => {
+      console.log('here', params.searchTerm);
+      console.log('tag', params.tag);
+      if (params.searchTerm)
+        this.foods = this.foodService.getAllFoodsBySearchTerm(
+          params.searchTerm
+        );
+      else if (params.tag)
+        this.foods = this.foodService.getAllFoodsByTag(params.tag);
+      else this.foods = this.foodService.getAll();
+    });
   }
 }
